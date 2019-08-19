9261d32fd9803e91a0244fb36e56a98ac5fb2218
Aryan <aryanpn@gmail.com>
2019-08-19 16:14:42 -0400

    I believe this will work for a tank drive

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,18 +1,23 @@
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 public class DriveSubsystem extends Subsystem{
     private Spark m_frontLeft  = new Spark(4);
     private Spark m_rearLeft   = new Spark(3);
     private Spark m_frontRight = new Spark(2);	
     private Spark m_rearRight  = new Spark(1);
-    private MecanumDrive m_robotDrive = new MecanumDrive(m_frontLeft,m_rearLeft,m_frontRight,m_rearRight);
+
+    SpeedControllerGroup left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+	SpeedControllerGroup right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+    private DifferentialDrive m_robotDrive = new DifferentialDrive(left, right);
     @Override
     protected void initDefaultCommand() {
         m_frontLeft.setInverted(true);
     }
     public void drive(double x, double y, double theta){
-        m_robotDrive.driveCartesian(x, y, theta);
+        m_robotDrive.arcadeDrive(y, theta);
     }
 }
